ddc8206beaa93577e4eca54d3750fa04af09fde6
taegyeong <rlaxorud7894@naver.com>
2020-06-24 13:42:06 +0900

    pug

--- app.js
+++ app.js
@@ -8,19 +8,21 @@ import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
 import routes from "./routes";
+import { localsMiddleware } from "./middlewares";
 const app = express();
 
+app.use(helmet());
 app.set("view engine", "pug");
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(helmet());
 app.use(morgan("dev"));
+app.use(localsMiddleware);
 // middleware 들을 거치고 마지막에 url
 
 app.use(routes.home, globalRouter)
 app.use(routes.users, userRouter); // localhost:4001/user/ ~~
-app.use("/video", videoRouter);
+app.use(routes.videos, videoRouter);
 
 export default app;
 /*
